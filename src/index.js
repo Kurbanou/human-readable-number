b11d61623ed859192eb0b449c5e848a7ea9c7ffc
Farid Kurbanou <kurbanou.faridun@gmail.com>
2022-01-11 00:03:43 +0300

    rwr

--- src/index.js
+++ src/index.js
@@ -14,29 +14,20 @@ module.exports = function toReadable (number) {
 
     num = number.toString()
 
-    if (number >= 100) { let h = hundreds[num[0]] + " "}
-    else {h = ""};
-
-    if (number < 100 && n > 20) { let t = tens[num[0]] + " "}
-    else {t = ""};
-
-    if (number < 10) { let u = units[num.length]};
-
-    if (number = 10) {  let b = 'ten'}
-
-    if (number < 20 && number > 10) { let b = zzz[num[1].toString() + 1]}
-
-    if (number = 0){let b = 'zerro'}
-
-    let b = h + t + u;
-
-
-
-    return b
-
-
-
-
+    if (number = 0){ return 'zero'}
+    else if (number = 100){ return 'one hundred'}
+    else if (number = 10) {  return  'ten'}
+    else if (number < 10) { return units[number]}
+    else if (number < 20 && number > 10) { return zzz[num[1]]}
+    else if (number < 100 && n > 20) { let t = tens[num[0]] + " "; let u = units[num[1]]; return t + u}
+    else if (number > 100) { let h = hundreds[num[0]] + " "; let t = tens[num[1]] + " "; let u = units[num[2]]; return h + t + u }
 
 
 }
+
+
+
+
+
+
+
