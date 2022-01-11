1aa7c4c712a6dfd91f12b6ea61d2b38c51d88db8
Farid Kurbanou <kurbanou.faridun@gmail.com>
2022-01-11 15:48:05 +0300

    etrer

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ module.exports = function toReadable (number) {
 
     const hundreds = ["", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
     const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    const units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    const zzz = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+    const zzz = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
 
     let h = "";
     let t = "";
@@ -14,36 +14,28 @@ module.exports = function toReadable (number) {
 
     num = number.toString()
 
-    if (number = 0){ return 'zero'}
+    
 
     if(num.length === 3 ){
 
-        if(num[1] > "19") {return hundreds[num[0]] + " " + tens[num[1]] + " " + units[num[2]]}
-        if( num[1] < "19" && num[1] > "10") {return hundreds[num[0]] + " " + zzz[num[2]] + " "}
+        if(num[1] === "0" && num[2] === "0") {return hundreds[num[0]]}
+        else if(num[1] === "0" && num[2] != "0") {return hundreds[num[0]] + " " + units[num[2]]}
+        else if(num[1] === "1" && num[2] != "0") {return hundreds[num[0]] + " " + zzz[num[2]]}
+        else if(num[1] > "19") {return hundreds[num[0]] + " " + tens[num[1]] + " " + units[num[2]]}       
+    }
+    if(num.length === 2 ){
 
+        if(num[0] === "0" && num[1] === "0") {return 'zero'}
+        else if(num[0] === "0" && num[1] !== "0") {return units[num[1]]}
+        else if(num[0] === "1" && num[1] !== "0") {return zzz[num[1]]}
+        else if(num[0] === "19") {return tens[num[0]] + " " + units[num[1]]}
+    }
+    if(num.length === 1 ){
 
+        return units[num[0]]}        
     }
 
   
-
-
-
-
-
-    // if (number = 0){ return 'zero'}
-    // else if (number = 100){ return 'one hundred'}
-    // else if (number = 10) {  return  'ten'}
-    // else if (number < 10) { return units[number]}
-    // else if (number < 20 && number > 10) { return zzz[num[1]]}
-    // else if (number < 100 && n > 20) { let t = tens[num[0]] + " "; let u = units[num[1]]; return t + u}
-    // else if (number > 100) { let h = hundreds[num[0]] + " "; let t = tens[num[1]] + " "; let u = units[num[2]]; return h + t + u }
-
-
-}
-
-
-
-
 
 
 
